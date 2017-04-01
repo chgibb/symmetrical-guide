995b079edd95f8d1b3bbb97ab7262788ae9741ac
scs97liu <shane1997liu@gmail.com>
2017-04-01 14:34:37 -0400

    Fixed Punch Animation Loop

--- src/level1.ts
+++ src/level1.ts
@@ -46,7 +46,7 @@ export class Level1 extends Phaser.State
         //this.player.sprite.animations.add('jump', [2], 1, true);
         //this.player.sprite.animations.add('run', [3,4,5,6,7,8], 7, true);
         this.player.sprite.animations.add('run', ['dude1', 'dude2', 'dude3', 'dude4', 'dude5', 'dude6', 'dude5', 'dude4', 'dude3', 'dude2', 'dude1', 'dude1'], 20, true);
-        this.player.sprite.animations.add('punch', ['dude1', 'dude2', 'dude3', 'dude4', 'dude5', 'dude6', 'dude5', 'dude4', 'dude3', 'dude2', 'dude1', 'dude1'], 20, true);
+        this.player.sprite.animations.add('punch', ['dude1', 'dude2', 'dude3', 'dude4', 'dude5', 'dude6', 'dude5', 'dude4', 'dude3', 'dude2', 'dude1', 'dude1'], 20, false);
         this.physics.enable(this.player.sprite,Phaser.Physics.ARCADE);
         this.camera.follow(this.player.sprite);
         this.player.sprite.body.collideWorldBounds = true;
