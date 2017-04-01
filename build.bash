5faaf096aaa44b212851ab4874f4b31a2548e850
Chris Gibb <chris.gibb@outlook.com>
2017-04-01 16:03:03 -0400

    Added more perf passes

--- build.bash
+++ build.bash
@@ -2,8 +2,16 @@
 
 ./node_modules/.bin/browserify index.js --node --debug -o dist.js
 
-./node_modules/nwsjs/nwsjs dist.js > tmp
+./node_modules/.bin/bundle-collapser dist.js > tmp
+mv tmp dist.js
 
+./node_modules/.bin/optimize-js dist.js > tmp
+mv tmp dist.js
+
+./node_modules/.bin/semi add dist.js --silent
+
+
+./node_modules/nwsjs/nwsjs dist.js > tmp
 mv tmp dist.js
 
 for f in $(find src -name '*.ts'); 
