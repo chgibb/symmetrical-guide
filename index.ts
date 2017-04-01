5c1cf7b4c268e7e4e9f9d2472f93d91b8a9c9fb4
Chris Gibb <chris.gibb@outlook.com>
2017-04-01 10:28:06 -0400

    Added sky and platform file

--- index.ts
+++ index.ts
@@ -15,10 +15,15 @@ import {game,
 
 function preload()
 {
-
+    game.load.image("sky","assets/sky.png");
+    game.load.image("star","assets/star.png");
+    game.load.spritesheet("dude","assets/dude.png",32,48);
 }
 function create()
 {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.add.sprite(0,0,"sky");
+
 }
 function update()
 {
